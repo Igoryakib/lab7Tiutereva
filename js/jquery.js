477e87fcb72bd62268b006964f448ad930b4b269
Igoryakib <igoryakib@gmail.com>
2024-10-13 15:27:51 +0300

    f commit

---
+++ js/jquery.js
@@ -0,0 +1,16 @@
+$( function() {
+    $( "#slider" ).slider();
+  } );
+
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  $( function() {
+    $( document ).tooltip();
+  } );
+  $( function() {
+    $( "#tabs" ).tabs();
+  } );
+  $( function() {
+    $( "#menu" ).menu();
+  } );
